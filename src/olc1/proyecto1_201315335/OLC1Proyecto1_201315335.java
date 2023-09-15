b70df08ddb8a148aa0d8d5432e95a28e1b35c75d
jeysel9 <lilithvanth@gmail.com>
2023-09-15 17:29:17 -0600

    cargas

---
+++ src/olc1/proyecto1_201315335/OLC1Proyecto1_201315335.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package olc1.proyecto1_201315335;
+
+/**
+ *
+ * @author lilit
+ */
+public class OLC1Proyecto1_201315335 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        ventana v = new ventana ();
+        v.setVisible(true);
+    }
+    
+}
